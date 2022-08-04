1860961ca65ec1ddd5339469b64e6c01041d5295
geraltang <geraltangsmile@gmail.com>
2022-08-04 22:57:10 +0930

    0804

--- 0731/script1.js
+++ 0731/script1.js
@@ -1,4 +1,24 @@
 console.log("asdsd")
 
+ul.addeventlistener 
+
 const co11n = document.querySelector('.co11n')
-co11n.innerHTML = 'tssdfsdfest'
+co11n.innerHTML = 'test from js'
+
+let qtime = 100
+console.log(qtime > 100 ? 'good' : 'bad')
+
+let time = 6
+if (time > 18 && time < 24) {
+    console.log('night')
+} else {
+    console.log('day')
+}
+
+let daytime = 7
+if ((daytime > 18 && daytime < 24) || (daytime > 0 && daytime < 6)) {
+    console.log('truenight')
+} else {
+    console.log('day')
+}
+

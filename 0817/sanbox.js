84b43b95db2eb034a974092fe758f37f59e615ae
geraltang <geraltangsmile@gmail.com>
2022-08-18 22:04:05 +0930

    0818

---
+++ 0817/sanbox.js
@@ -0,0 +1,73 @@
+// var student;
+
+
+// student = 'ABC';
+
+
+// var student1, student2, student3;
+
+// var a = 0;
+
+// car = "ABC"
+// console.log(car)
+// var car;
+
+// var amIAGuy = true;
+// var amIAWoman = false
+
+// var age = undefined;
+// var gender = null;
+
+// var teacher = {};
+// var gender = "gender"
+// teacher = {
+//     teacherName: 'ABC',
+//     age: 18,
+//     gender: "Male",
+//     onDuty: true
+// }
+// teacher.age = 20
+// teacher['gender'] = ['male']
+
+// console.log(teacher);
+// console.log(teacher.age);
+// console.log(teacher[gender]);
+
+// var students = ['Jing', 'Emily', 'Holly', 'Shawn'];
+// console.log(students[2])
+
+// var concat = "2" + "3";
+// console.log(concat)
+
+// var names = ['Jing','Emily','Holly','Jack'];
+// names.push('Tom');
+// names.pop();
+// names[2] = 'Tom'
+// console.log(names);
+
+// var twoDimentionalArray = [[1,2,3],[4,5,6],[7,8,9]];
+// console.log(twoDimentionalArray[1][2]);
+
+// var age = 18
+// console.log(age < 18 ? "over": "under");
+
+// for (var i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+// for (var i = 0; i < 10; i++){
+//     for(var j = 0; j < 10; j++){
+//         console.log('hello');
+//     }
+// }
+
+// var i = 0
+// while (i < 10){
+//     console.log(i);
+//     i++;
+//     if (i===5){
+//         i++;
+//         break;
+//     }
+// }
+
